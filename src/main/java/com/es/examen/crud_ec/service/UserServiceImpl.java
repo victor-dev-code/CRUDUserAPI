5ce97c99d5989112fc8f074294650bfc51926b55
Victor Hdez <hdezyvictor99@gmail.com>
2025-09-04 19:32:59 -0600

    se agrega el logger para colocar mensajes en el proyecto

--- src/main/java/com/es/examen/crud_ec/service/UserServiceImpl.java
+++ src/main/java/com/es/examen/crud_ec/service/UserServiceImpl.java
@@ -11,7 +11,9 @@ import com.es.examen.crud_ec.entity.User;
 import com.es.examen.crud_ec.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
@@ -20,14 +22,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public ResponseUserDTO saveUser(RequestUserDTO userDTO) {
+        log.info("Agregando datos del nuevo usuario con nombre: {}", userDTO.getUsername());
+
         User userEntity = modelMapper.map(userDTO, User.class);
         return modelMapper.map(userRepository.save(userEntity), ResponseUserDTO.class);
     }
 
     @Override
     public Optional<ResponseUserDTO> getById(Long id) {
+        log.info("Se busca usuario con el id {}", id);
         return userRepository.findById(id).map(
             user -> {
+                log.info("Se ha encontrado el usuario {}", user.getUsername());
                 return modelMapper.map(user, ResponseUserDTO.class);
             }
         );
@@ -35,9 +41,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Optional<ResponseUserDTO> updateUser(RequestUserDTO userDTO) {
+        log.info("Se busca el usuario con el id {}", userDTO.getId());
         User user = modelMapper.map(userDTO, User.class);
         return userRepository.findById(userDTO.getId()).map(
             userDB -> {
+                log.info("Se encuentra el usuario {} ", userDB.getUsername());
                 return modelMapper.map(userRepository.save(user), ResponseUserDTO.class);
             }
         );
@@ -45,9 +53,11 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public Boolean deleteById(Long id) {
+        log.info("Se busca el usuario con el id {}", id);
         return userRepository.findById(id).map(
             user -> {
                 userRepository.delete(user);
+                log.info("Se elimina el usuario solicitado");
                 return true;
             }
         ).orElse(false);
